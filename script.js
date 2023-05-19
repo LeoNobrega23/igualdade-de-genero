600c51b9bb45d0d6050990ec97169e08de8a1e33
Leonardo Nobrega <leonardo.nobrega56@gmail.com>
2023-05-19 11:01:41 -0300

    teste slide

---
+++ script.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 1,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+    spaceBetween: 19,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 19,
+    },
+    991: {
+      slidesPerView: 1,
+    },
+    1200: {
+      slidesPerView: 1,
+    }
+
+  }});
